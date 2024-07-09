78c08e423225d780c23c963f8db46f68c9fc79bc
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-07-09 12:37:26 +0200

    fix(03-reverseString): change method to array instead of for loop

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,9 +1,5 @@
-const reverseString = function(string) {
-  let newString = '';
-  for (let i = string.length - 1; i >= 0; i--) {
-    newString += string[i];
-  }
-  return newString
+const reverseString = function (string) {
+  return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
