f78395a0f4fe6f6739241d7e9b04952ea0cbbada
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-07-09 14:50:28 +0200

    solve(o4-removeFromArray)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+  for (let i = 0 ; i < arg.length ; i++) {
+    let j = 0;
+    while( j < array.length) {
+      if (array[j] === arg[i]) {
+        array.splice(j, 1);
+      } else {
+        j++;
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
