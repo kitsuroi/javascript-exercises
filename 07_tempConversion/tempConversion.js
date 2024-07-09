fbabe956782fb43201f4278ce4b958f505640e47
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-07-09 17:27:38 +0200

    fix(07-tempConversion): change method to Math.round + renamed x to fahrenheit and celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const convertToCelsius = function(x) {
-  return Number(((x-32)*(5/9)).toFixed(1));
+const convertToCelsius = function(fahrenheit) {
+  return Math.round(( (fahrenheit - 32) * (5/9)) * 10 ) / 10;
 };
 
-const convertToFahrenheit = function(x) {
-  return Number((x * (9/5) + 32).toFixed(1));
+const convertToFahrenheit = function(celsius) {
+  return Math.round(( celsius * (9/5) + 32) * 10 ) / 10;
 };
 
 // Do not edit below this line
