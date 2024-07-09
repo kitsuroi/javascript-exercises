37bd3c1110de241ac33322b1e877ef220795c877
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-07-09 15:24:31 +0200

    fix(04-removeFromArray): change to array methods instead of for loops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,5 @@
 const removeFromArray = function(array, ...arg) {
-  for (let i = 0 ; i < arg.length ; i++) {
-    let j = 0;
-    while( j < array.length) {
-      if (array[j] === arg[i]) {
-        array.splice(j, 1);
-      } else {
-        j++;
-      }
-    }
-  }
-  return array;
+  return array.filter( val => !arg.includes(val))
 };
 
 // Do not edit below this line
