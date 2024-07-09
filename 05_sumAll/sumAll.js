973ea573a485ceb07592040b90bb46defcf563cf
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-07-09 15:51:39 +0200

    solve(05-sumAll)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  if (num1 < 0 || num2 < 0){
+    return "ERROR";
+  } else if (typeof num1 != 'number' || typeof num2 != 'number') {
+    return "ERROR";
+  } else if (num1 > num2) {
+    let a = num1;
+    num1 = num2;
+    num2 = a;
+  }
+  while (num1 <= num2){
+    sum += num1;
+    num1++;
+  }
+  return sum;
 };
 
 // Do not edit below this line
