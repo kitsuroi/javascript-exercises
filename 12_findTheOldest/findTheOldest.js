2507de7c4c2b38891992a8c7f37d004e3277f49c
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-08-07 19:01:40 +0200

    solve(12-findTheOldest)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const getAge = function(person){
+  person.yearOfDeath ??= (new Date()).getFullYear();
+  return person.yearOfDeath - person.yearOfBirth;
+};
 
+const findTheOldest = function(people) {
+  people.sort(function(lastPerson, nextPerson){
+    return getAge(lastPerson) > getAge(nextPerson) ? -1 : 1;
+  });
+  return people[0];
 };
 
 // Do not edit below this line
