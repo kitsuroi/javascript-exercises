be649d2d28986ca67b2c1fae476fa7119325ff51
kitsuroi <154440204+kitsuroi@users.noreply.github.com>
2024-08-07 17:22:19 +0200

    solve(10-fibonacci)

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 0) return "OOPS";
+  if (n == 0) return 0;
+  if (n <= 2) return 1;
+  let a = 1, b = 1;
+  for (let i = 3; i <= n; i++){
+    temp = a;
+    a += b;
+    b = temp;
+  };
+  return a;
 };
 
 // Do not edit below this line
